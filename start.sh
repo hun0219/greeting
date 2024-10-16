3b97caa07abdc36b8765ff09b71e1181924bd3f7
hun0219 <gsw98000@gmail.com>
2024-10-16 18:29:34 +0900

    ok

--- start.sh
+++ start.sh
@@ -4,8 +4,10 @@
 # nohup java \
 #	-jar build/libs/greeting-0.0.1-SNAPSHOT.jar &
 JAR_PATH="build/libs/greeting-0.0.1-SNAPSHOT.jar"
+LOG_DIR="logs"
 
 # JAR 파일 실행
 nohup java -jar $JAR_PATH > logs/app.log 2>&1 &
 
+echo $! > $LOG_DIR/app.pid
 echo "> Application started with PID $!"
