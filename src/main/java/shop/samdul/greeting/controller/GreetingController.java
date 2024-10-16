8808d7fcf7ffef1a6d2a1ecdd0714a187dc77ba6
hun0219 <gsw98000@gmail.com>
2024-10-16 15:07:58 +0900

    add peple num, name OK

--- src/main/java/shop/samdul/greeting/controller/GreetingController.java
+++ src/main/java/shop/samdul/greeting/controller/GreetingController.java
@@ -20,30 +20,33 @@ public class GreetingController {
                     ) String name, int num, Model model) {
             System.out.println(name);
             System.out.println(num);
+                        
+            // Stack 누적/ 전역 Stack 안으로 
+            //Stack<People> peopleStack = new Stack<>();
+            
+            // 이름의 첫 글자를 대문자로 변환
+            if (name != null && !name.isEmpty()) {
+                name = Character.toUpperCase(name.charAt(0)) + name.substring(1);
+            }
 
-        // 이름의 첫 글자를 대문자로 변환
-        if (name != null && !name.isEmpty()) {
-            name = Character.toUpperCase(name.charAt(0)) + name.substring(1);
-        }
+            // 아래를 안쓰려면 People.java 7-9라인 , 변수에 대한 타입 선언해줘야함
+            // People people1 = new People(num, name);
+            People people1 = new People();
 
-        // 아래를 안쓰려면 People.java 7-9라인 , 변수에 대한 타입 선언해줘야함
-        // People people1 = new People(num, name);
-        People people1 = new People();
+            // 상속 Woker 클래스
+            //People people1 = new Woker();
+            
+            // People.java에 people이 public으로 선언되어있기 때문에 아래를 쓸 수 있음
+            // 보통 변수는 public으로 잘안씀
+            people1.num=num;
+            people1.name=name;
 
-        // 상속 Woker 클래스
-        //People people1 = new Woker();
-        
-        // People.java에 people이 public으로 선언되어있기 때문에 아래를 쓸 수 있음
-        // 보통 변수는 public으로 잘안씀
-        people1.num=num;
-        people1.name=name;
+            
+            // 스택에 People 객체 추가
+            peopleStack.push(people1);
 
-
-        // 스택에 People 객체 추가
-        peopleStack.push(people1);
-
-        // 모델에 스택 전체 추가
-        model.addAttribute("peopleStack", peopleStack);
+            // 모델에 스택 전체 추가
+            model.addAttribute("peopleStack", peopleStack);
 
         return "greeting";
     }
