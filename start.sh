038ca40d397989de7275bd863c94f0b7ed0104d5
hun0219 <gsw98000@gmail.com>
2024-10-16 18:43:21 +0900

    ok

--- start.sh
+++ start.sh
@@ -4,10 +4,18 @@
 # nohup java \
 #	-jar build/libs/greeting-0.0.1-SNAPSHOT.jar &
 JAR_PATH="build/libs/greeting-0.0.1-SNAPSHOT.jar"
-LOG_DIR="logs"
+#LOG_DIR="logs"
 
 # JAR 파일 실행
-nohup java -jar $JAR_PATH > logs/app.log 2>&1 &
+#nohup java -jar $JAR_PATH > logs/app.log 2>&1 &
 
-echo $! > $LOG_DIR/app.pid
+#echo $! > $LOG_DIR/app.pid
+#echo "> Application started with PID $!"
+
+
+set +e #so "at now" will run even if java -jar fails
+
+#Run java app in background
+#echo "java -jar $(ls | grep *.jar | head -n 1)a" | at now
+echo "java -jar $JAR_PATH" | at now
 echo "> Application started with PID $!"
