9ace237f0e8d0036394161e29ed2712e91d9344a
hun0219 <gsw98000@gmail.com>
2024-10-16 18:16:30 +0900

    logs add

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@
 JAR_PATH="build/libs/greeting-0.0.1-SNAPSHOT.jar"
 
 # JAR 파일 실행
-nohup java -jar $JAR_PATH > build/libs/app.log 2>&1 &
+nohup java -jar $JAR_PATH > logs/app.log 2>&1 &
 
 echo "> Application started with PID $!"
